35ab75069a6c7b06a24514815c8e071dc1cdf874
Suresh_Prabha <suresh.uday92@gmail.com>
2021-06-18 13:05:09 +0530

    Changes

--- src/test/java/com/springboot/project/reservationservice/ReservationServiceApplicationTests.java
+++ src/test/java/com/springboot/project/reservationservice/ReservationServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.springboot.project.reservationservice;
 
 import com.springboot.project.reservationservice.entity.BookingStatus;
 import com.springboot.project.reservationservice.entity.Reservation;
+import com.springboot.project.reservationservice.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -36,7 +39,7 @@ class ReservationServiceApplicationTests {
 		HttpEntity<Reservation> entity = new HttpEntity<>(reservation);
 		ResponseEntity<String> responseEntity = testRestTemplate.exchange("/reservations", HttpMethod.POST, entity,
 				String.class);
-		Assertions.assertEquals(responseEntity.getStatusCodeValue(),HttpStatus.CREATED.value());
+		Assertions.assertEquals(HttpStatus.CREATED.value(),responseEntity.getStatusCodeValue());
 	}
 
 	@Test
@@ -45,7 +48,46 @@ class ReservationServiceApplicationTests {
 		HttpEntity<Reservation> entity = new HttpEntity<>(reservation);
 		ResponseEntity<String> responseEntity = testRestTemplate.exchange("/reservations", HttpMethod.POST, entity,
 				String.class);
-		Assertions.assertEquals(responseEntity.getStatusCodeValue(),HttpStatus.INTERNAL_SERVER_ERROR.value());
+		Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(),responseEntity.getStatusCodeValue());
+	}
+
+	@Test
+	void testGetReservedRooms(){
+		ReservationDetails reservation = ReservationDetails.builder().reservationId("12c9f1e4a1a04878860b588f42d37a6d")
+				.hotelId("12c9f1e4a1a04878860b588f42d37a6d").guestId("12c9f1e4a1a04878860b588f42d37a6d")
+				.reservationFromDate(new Date()).reservationToDate(new Date()).bookingStatus(BookingStatus.BOOKED.toString()).build();
+		ResponseEntity<ReservationDetails[]> responseEntity = testRestTemplate
+				.getForEntity("/reservations/reservedRooms?fromDate=2020-12-12&toDate=2021-12-31", ReservationDetails[].class);
+		Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+	}
+
+	@Test
+	void testGetAvailableRoomForReservation(){
+		ReservationDetails reservation = ReservationDetails.builder().reservationId("12c9f1e4a1a04878860b588f42d37a6d")
+				.hotelId("12c9f1e4a1a04878860b588f42d37a6d").guestId("12c9f1e4a1a04878860b588f42d37a6d")
+				.reservationFromDate(new Date()).reservationToDate(new Date()).bookingStatus(BookingStatus.BOOKED.toString()).build();
+		ResponseEntity<ReservationDetails> responseEntity = testRestTemplate
+				.getForEntity("/reservations/availableReservation?fromDate=2020-12-12&toDate=2021-12-31&roomId=1", ReservationDetails.class);
+		Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+	}
+
+	@Test
+	void testGetReservationSummary(){
+		AddressDetails address = AddressDetails.builder().id(1).addressType("HOME").build();
+		GuestDetails guest = GuestDetails.builder().guestId("12c9f1e4a1a04878860b588f42d37a6d")
+				.guestName("Name").addressDetails(Arrays.asList(address)).build();
+		RoomDetails room = RoomDetails.builder().roomId(1).roomType("KING_BED").build();
+		HotelDetails hotel = HotelDetails.builder().hotelId("12c9f1e4a1a04878860b588f42d37a6d")
+				.hotelName("Name").roomDetails(Arrays.asList(room)).build();
+		ReservationSummary reservationSummary = ReservationSummary.builder().reservationId("12c9f1e4a1a04878860b588f42d37a6d")
+				.reservationFromDate(new Date()).reservationToDate(new Date()).bookingStatus(BookingStatus.BOOKED.toString())
+				.hotelDetails(hotel).guestDetails(guest).build();
+
+		testRestTemplate.execute("/reservations/12c9f1e4a1a04878860b588f42d37a6d/reservationSummary",
+				HttpMethod.GET,null, null);
+		ResponseEntity<ReservationSummary> responseEntity = testRestTemplate
+				.getForEntity("/reservations/12c9f1e4a1a04878860b588f42d37a6d/reservationSummary", ReservationSummary.class);
+		Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,responseEntity.getStatusCode());
 	}
 
 }
